5d9755c625281534f68f6c66080efd7ffa9cf0a6
laravelProgrammer <laravelprogrammers@gmail.com>
2019-03-13 21:32:49 -0700

    php artisan make:model bla bla -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Product Controller
+Route::apiResource('/products','ProductController');
+
+// Review Controller 
+
+Route::group(['prefix'=>'/products'],function(){
+   Route::apiResource('/{product}/reviews','ReviewController');
+});
+
+
+
