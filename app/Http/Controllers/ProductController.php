ac72e7e383cc6f7b3ff05c111e268237fda34f67
laravelProgrammer <laravelprogrammers@gmail.com>
2019-03-16 20:26:07 -0700

    Product updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Requests\Product\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct(){
+          $this->middleware('auth:api')->except('index','show');                            
+    }
     public function index()
     {
         return ProductCollection::collection(Product::paginate(10));
@@ -34,9 +34,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+       $product=new Product();
+       $product->name=$request->name;
+       $product->detail=$request->detail;
+       $product->price=$request->price;
+       $product->stock=$request->stock;
+       $product->discount=$request->discount;
+       $product->save();
+       return response([
+                'data'=> new ProductResource($product)         
+            ],Response::HTTP_CREATED);
+
     }
 
     /**
@@ -71,7 +81,16 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //return $product=Product::find($product);
+        $product->name=$request->name;
+        $product->detail=$request->detail;
+        $product->price=$request->price;
+        $product->stock=$request->stock;
+        $product->discount=$request->discount;
+        $product->save();
+        return response([
+            'data'=> new ProductResource($product)         
+        ]);
     }
 
     /**
